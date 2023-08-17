6192801155f0c5d0a9335275cfa3f9b86d68310d
Lewis <lewis.murgor@student.moringaschool.com>
2023-08-17 16:43:47 +0300

    add custom hooks

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,4 +1,3 @@
-import { useEffect, useState, useMemo } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
 import './main-page.css';
 import Header from './header';
@@ -6,45 +5,35 @@ import FeaturedHouse from './featured-house';
 import SearchResults from '../search-results';
 import HouseFilter from './house-filter';
 import HouseFromQuery from '../house/HouseFromQuery';
+import useHouses from '../hooks/useHouses';
+import useFeaturedHouse from '../hooks/useFeaturedHouse';
+import HousesContext from '../context/housesContext';
 
 function App() {
-  const [allHouses, setAllHouses] = useState([]);
-  useEffect(() => {
-    const fetchHouses = async () => {
-      const rsp = await fetch("/houses.json");
-      const houses = await rsp.json();
-      setAllHouses(houses);
-    };
-    fetchHouses();
-  }, []);
+  const allHouses = useHouses();
+  const featuredHouse = useFeaturedHouse(allHouses);
 
-  const featuredHouse = useMemo(() => {
-    if(allHouses.length) {
-      const randomIndex = Math.floor(Math.random() * allHouses.length);
-      return allHouses[randomIndex];
-    }
-  }, [allHouses]);
-
-  
   return (
     <Router>
+      <HousesContext.Provider value={allHouses}>
       <div className='container'>
         <Header subtitle="Providing houses all over the world" />
-        <HouseFilter allHouses={allHouses}/>
+        <HouseFilter />
 
         <Switch>
           <Route path="/searchresults/:country">
-            <SearchResults allHouses={allHouses}/>
+            <SearchResults />
           </Route>
 
           <Route path="/house/:id">
-            <HouseFromQuery allHouses={allHouses}/>
+            <HouseFromQuery />
           </Route>
           <Route path="/">
             <FeaturedHouse house={featuredHouse}></FeaturedHouse>
           </Route>
         </Switch>
       </div>
+      </HousesContext.Provider>
     </Router>
   );
 }
